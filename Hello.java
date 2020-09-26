a86a54c3b34675f8f58640b412b68378f1d04a16
shizihui <shi56535874@163.com>
2020-09-26 21:24:40 +0800

    九月二十六号第一次作业

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("这是一个简单的Java程序");
+        Student stu = new Student();
+        stu.speak("We are students");
+
+    }
+}
+ class  Student{
+    public  void  speak(String s){
+        System.out.println(s);
+    }
+ }

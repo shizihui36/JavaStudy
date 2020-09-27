e0318a2b2d87eed4fc38a16555e9fb57d340ea45
shizihui <shi56535874@163.com>
2020-09-27 20:50:23 +0800

    九月二十七

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       int a[] = {1,2,3,4};
+       int b[] = {100,200,300};
+        System.out.println("数据a的元素个数 = " +a.length);
+        System.out.println("数据b的元素个数 = " +b.length);
+        System.out.println("数据a的引用 = " +a);
+        System.out.println("数据b的引用 = " +b);
+        System.out.println("a==b的结果是"+(a==b));
+        a=b;
+        System.out.println("数组a的元素个数="+a.length);
+        System.out.println("数据b的元素个数="+b.length);
+        System.out.println("a==b的结果是"+(a==b));
+        System.out.println("a[0]= "+a[0]+",a[1]="+a[1]+",a[2]="+a[2]);
+        System.out.print("b[0]="+a[0]+",b[1]="+b[1]+"+,b[2]="+b[2]);
+    }
+
+}
